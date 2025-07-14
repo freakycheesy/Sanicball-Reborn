ce29d22c6e4d0e0d4bd0d49e40f4811d2a372960
stoppedwumm <mobileboxnews@icloud.com>
2025-07-14 18:58:25 +0200

    stuff

--- Subprojects/Core/GameVersion.cs
+++ Subprojects/Core/GameVersion.cs
@@ -3,15 +3,15 @@
     public class GameVersion
     {
         //The current version as a float, for checking which version is newest
-        public const float AS_FLOAT = 0.82f;
+        public const float AS_FLOAT = 0.90f;
 
         //To differentiate between testing builds and release builds
         public const bool IS_TESTING = false;
 
         //As a string, for displaying on the UI
-        public const string AS_STRING = "alpha v0.8.2";
+        public const string AS_STRING = "alpha-revived v0.9.0";
 
         //Something stupid, usually unique for every version
-        public const string TAGLINE = "The best failed early access game";
+        public const string TAGLINE = "f*ck unity. and i mean that personally. - stoppedwumm";
     }
 }
